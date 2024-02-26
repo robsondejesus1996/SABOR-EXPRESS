847fedb2d7e7e0e94e30f722051cd5ad59b91d2b
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-26 10:36:19 -0300

    Utilização de laço de repetição para listar os restaurantes cadastrados

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 
-restaurantes = []
+restaurantes = ['Restaurante Pizza', 'Restaurante Sushi']
 
 def exibir_nome_do_programa():
     print("""
@@ -38,7 +38,16 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso')
-    input('Digite uma tecla para voltar ao menu principal')
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
+
+def listar_restaurantes():
+    os.system('cls')   
+    print('Listar todos restaurantes\n')
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    input('\nDigite uma tecla para voltar ao menu principal: ')
     main()
 
 def escolher_opcao():
@@ -48,7 +57,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listrar restaurantes')   
+            listar_restaurantes()   
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
