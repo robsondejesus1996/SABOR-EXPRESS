d22bf221bd69810bf081ef3dd9425c12e1729862
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-26 13:47:30 -0300

    Atualização de app

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
 import os
 
-
-restaurantes = ['Restaurante Pizza', 'Restaurante Sushi']
+restaurantes = [{'nome':'Praça', 'categoria': 'Japonesa', 'ativo':False},
+                {'nome': 'Pizza Suprema', 'categoria': 'Pizza', 'ativo': True},
+                {'nome': 'Cantina', 'categoria': 'Italiano', 'ativo': True}]
 
 def exibir_nome_do_programa():
     print("""
-        
-    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-        """)
-
-#https://fsymbols.com/pt/
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
 
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
-    print('2. Listar restaurante')
-    print('3. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
-   exibir_subtitulo('Finalizando app.')
+    exibir_subtitulo('Finalizar app')
 
 def voltar_ao_menu_principal():
-    input('\nDigite uma tecla para voltar para ao menu: ')
-    main()   
-
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
 
 def opcao_invalida():
-    print('Opção invalida!\n')   
+    print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
-
-def exibir_subtitulo(texto)    :
+def exibir_subtitulo(texto):
     os.system('cls')
     print(texto)
     print()
@@ -44,41 +40,41 @@ def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
-    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso')
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
+
     for restaurante in restaurantes:
         print(f'.{restaurante}')
 
     voltar_ao_menu_principal()
 
 def escolher_opcao():
-
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
-        if opcao_escolhida == 1:
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1: 
             cadastrar_novo_restaurante()
-        elif opcao_escolhida == 2:
-            listar_restaurantes()   
-        elif opcao_escolhida == 3:
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
             print('Ativar restaurante')
-        elif opcao_escolhida == 4:
-            finalizar_app() 
-        else:
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else: 
             opcao_invalida()
     except:
-        opcao_invalida()        
-
+        opcao_invalida()
 
 def main():
-   os.system('cls')
-   exibir_nome_do_programa()
-   exibir_opcoes()
-   escolher_opcao()
-   
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 if __name__ == '__main__':
     main()
-
