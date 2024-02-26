38357757d584eeb7e2fdda6e10562ac42f4bfba2
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-26 08:35:13 -0300

    try except

--- app.py
+++ app.py
@@ -23,19 +23,32 @@ def finalizar_app():
    os.system('cls')
    print('Finalizando o app')
 
+
+def opcao_invalida():
+    print('Opção invalida!\n')   
+    input('Digite uma tecla para voltar para ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listrar restaurantes')   
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')   
-    else:
-        finalizar_app()  
+
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listrar restaurantes')   
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app() 
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()        
 
 
 def main():
+   os.system('cls')
    exibir_nome_do_programa()
    exibir_opcoes()
    escolher_opcao()
